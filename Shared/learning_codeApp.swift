de6267175aff6dcf7907dc6d3c7591335ce9c6e8
WilliamB <boneyw@gmail.com>
2022-01-26 09:32:13 -0500

    Initial Commit

---
+++ Shared/learning_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_codeApp.swift
+//  Shared
+//
+//  Created by william boney on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct learning_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
